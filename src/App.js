799a0ad4468c1f5cb949fa1b33f7a05fbac7155b
Andy <nunuzuhh@gmail.com>
2021-11-18 16:24:16 -0800

    11/18: Clean up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
-// JSX - JavaScript XML
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ReactRouter from "./component/ReactRouter";
-// pages
-import Home from './pages/Home';
-import Giovanna from './pages/Giovanna';
-// navbar
-import Navbar from "./component/Navbar";
-import ScrollToTop from "./component/ScrollToTop";
-import About from "./pages/About";
 
 function App() {
   return (
